c951f5eb6d618e3286d8334ea8843b04f900f8ae
Sebastián <BICHOS1506@GMAIL.COM>
2025-03-26 23:13:43 +0000

    cartas aleatorio terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let elementH1 = document.getElementById("value");
+
+function obtenerAleatorio(arr) {
+  const indice = Math.floor(Math.random() * arr.length);
+  return arr[indice];
+}
+
+const numeros = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "A"]
+console.log(obtenerAleatorio(numeros));
+
+elementH1.innerHTML = obtenerAleatorio(numeros);
+
+
+
+
+let elementIcono = document.querySelectorAll ("span.icon-right, span.icon-left");
+
+function iconAleatorio (arr) {
+  const indice =Math.floor( Math.random() * arr.length); 
+  return arr[indice];
+}
+const iconArray2 =[{pinta: '♦', color: 'red'},{pinta: '♥', color: 'red'},{pinta: '♠', color: 'black'},{pinta: '♣', color: 'black'}]
+
+
+let icon = iconAleatorio (iconArray2);
+console.log ( icon)
+
+elementIcono.forEach(elemento => {
+  elemento.innerHTML = icon.pinta;
+  elemento.style.color = icon.color;
+});
+
+
+
